ea1b55896bddc16b7a3ccc9ca43af4b84e9e267b
Sampson <sampsondraw@gmail.com>
2021-10-18 19:43:26 +1300

    Adding user class

---
+++ backend/backend/Models/User.cs
@@ -0,0 +1,14 @@
+namespace backend
+{
+    public class User
+    {
+        public DateTime Date { get; set; }
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Password { get; set; }
+
+    }
+}

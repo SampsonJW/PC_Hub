1b223068331a17ae611e774253d617cc2a67a47e
Sampson <sampsondraw@gmail.com>
2021-10-21 18:44:19 +1300

    Re starting Auth API

---
+++ backend/AuthAPI/AuthAPI/Models/User.cs
@@ -0,0 +1,19 @@
+﻿namespace AuthAPI.Models
+{
+    public class User
+    {
+        string Id { get; set; }
+
+        DateTime? CreatedAt {  get; set; } = default(DateTime?);
+
+        DateTime? UpdatedAt {  get; set;} = default(DateTime?);
+
+        string FirstName { get; set; }
+
+        string LastName { get; set; }
+
+        string Email { get; set; }
+
+        string Password { get; set; }
+    }
+}

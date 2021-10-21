7b7d67ac22e8a721ed09902af34a48374c6b6f4a
Sampson <sampsondraw@gmail.com>
2021-10-21 19:22:24 +1300

    Setting up Auth API with SQL

---
+++ backend/AuthAPI/AuthAPI/Models/UserContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace AuthAPI.Models
+{
+    public class UserContext : DbContext
+    {
+        public UserContext()
+        {
+
+        }
+        public DbSet<User> Users {  get; set; }
+    }
+}
